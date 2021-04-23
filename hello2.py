6b7063a6f764c2a0f20097633dc573a8c3b29b41
OdetteZah <zahariaodette@gmail.com>
2021-04-23 13:36:33 +0100

    hello2

---
+++ hello2.py
@@ -0,0 +1,28 @@
+import requests
+
+def recipe_search(ingredient):
+
+    app_id = '46c678f9'
+    app_key = '98b90d6e3f70e675e33e5e31eaf13b1e'
+    result = requests.get(
+'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id, app_key)
+    )
+    data = result.json()
+
+    return data['hits']
+
+def run():
+    ingredient = input('Enter an ingredient: ')
+
+    results = recipe_search(ingredient)
+
+    for result in results:
+        recipe = result['recipe']
+
+
+        print(recipe['label'])
+        print(recipe['uri'])
+        print()
+
+
+run()
