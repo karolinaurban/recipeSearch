12cb9d2df9603ac7da74f2eefe287e7b8fcc2147
OdetteZah <zahariaodette@gmail.com>
2021-04-26 01:33:56 +0100

    hello2

--- hello2.py
+++ hello2.py
@@ -1,15 +1,15 @@
 import requests
 
+
 def recipe_search(ingredient):
 
     app_id = '46c678f9'
     app_key = '98b90d6e3f70e675e33e5e31eaf13b1e'
-    result = requests.get(
-'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id, app_key)
-    )
+    result = requests.get('https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id, app_key))
     data = result.json()
 
     return data['hits']
+
 
 def run():
     ingredient = input('Enter an ingredient: ')
@@ -19,10 +19,24 @@ def run():
     for result in results:
         recipe = result['recipe']
 
-
-        print(recipe['label'])
+        print('\n' + recipe['label'])
         print(recipe['uri'])
-        print()
 
 
 run()
+
+class saverecipes:
+
+    saverecipes = input('Do you want to save these recipes into a file?')
+
+if saverecipes == 'y':
+    with open("recipe.txt", "w") as recipe_file:
+        recipe_file.write(saverecipes) + int('save_recipe')
+        recipe_file.close()
+
+else:
+    print('Thank you! Enjoy your meal')
+
+file2write=open("Your recipes",'w')
+file2write.write("Your recipes are here, enjoy!")
+file2write.close()
