648f49efda140af59e5cfee33e05709c3e8d45ab
karolinaurban <56004188+karolinaurban@users.noreply.github.com>
2021-05-03 20:29:12 +0100

    final version

--- base_code.py
+++ base_code.py
@@ -13,34 +13,44 @@ def recipe_search(ingredient):
 
 def save_recipes(ingredient, results):
     choice = input('\nDo you want to save these recipes? ')
+    results = recipe_search(ingredient)
 
-    if choice == 'y':
-        file = open('recipes.txt', 'w+')
+    if choice == 'y' or choice == 'Yes' or choice == 'yes' or choice == 'YES':
+        file = open('recipes.txt', 'a+')
         try:
-            results = recipe_search(ingredient)
+            file.write('Saved recipes for {}:'.format(ingredient))
             for result in results:
                 recipe = result['recipe']
 
-                file.write(recipe['label'])
+                file.write('\n' + recipe['label'])
+                file.write('\nCooking time:' + str(recipe['totalTime']))
+                file.write('\n' + 'Diet labels:' + str(recipe['dietLabels']))
+                file.write('\n' + 'Health labels:' + str(recipe['healthLabels']))
+                file.write('\n' + recipe['shareAs'] + '\n')
+
+            file.write('\n')
         finally:
             file.close()
         print('Done! Thank you! Enjoy your meal.')
+    else:
+        print('Okay, that\'s it then. Enjoy!')
 
 
 def run():
     ingredient = input('Enter an ingredient: ')
+    # in theory, you can also search for a certain cuisine, diet (i.e. vegan) or meal type
+    # and it will still display recipes that match that criteria
 
     results = recipe_search(ingredient)
     for result in results:
         recipe = result['recipe']
         print('\n' + recipe['label'])
-        print(recipe['dietLabels'])
-        print(recipe['healthLabels'])
+        print('\nCooking time:' + str(recipe['totalTime']))
+        print('Diet labels:' + str(recipe['dietLabels']))
+        print('Health labels:' + str(recipe['healthLabels']))
         print(recipe['shareAs'])
 
     save_recipes(ingredient, results)
 
 
 run()
-
-
